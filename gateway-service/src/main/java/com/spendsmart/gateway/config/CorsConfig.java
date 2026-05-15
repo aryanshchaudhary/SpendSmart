d6b009d56801b1b201ff268b0abc765577759b6e
Aryansh Chaudhary <abhichaudhary012002@gmail.com>
2026-05-15 22:31:14 +0530

    [Aryansh Chaudhary] Fixed Cors issues

--- gateway-service/src/main/java/com/spendsmart/gateway/config/CorsConfig.java
+++ gateway-service/src/main/java/com/spendsmart/gateway/config/CorsConfig.java
@@ -1,4 +1,34 @@
+//package com.spendsmart.gateway.config;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.cors.reactive.CorsWebFilter;
+//
+//@Configuration
+//public class CorsConfig {
+//
+//    @Bean
+//    public CorsWebFilter corsWebFilter() {
+//
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.addAllowedOrigin("http://localhost:4200");
+//        config.addAllowedOrigin("https://spend-smart-frontend-silk.vercel.app");
+//        config.addAllowedMethod("*");
+//        config.addAllowedHeader("*");
+//        config.setAllowCredentials(true);
+//
+//        UrlBasedCorsConfigurationSource source =
+//                new UrlBasedCorsConfigurationSource();
+//
+//        source.registerCorsConfiguration("/**", config);
+//
+//        return new CorsWebFilter(source);
+//    }
+//}
+
 package com.spendsmart.gateway.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -12,11 +42,14 @@ public class CorsConfig {
     public CorsWebFilter corsWebFilter() {
 
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://localhost:4200");
-        config.addAllowedOrigin("https://spend-smart-frontend-silk.vercel.app");
-        config.addAllowedMethod("*");
-        config.addAllowedHeader("*");
+
         config.setAllowCredentials(true);
+
+        config.addAllowedOriginPattern("*");
+
+        config.addAllowedHeader("*");
+
+        config.addAllowedMethod("*");
 
         UrlBasedCorsConfigurationSource source =
                 new UrlBasedCorsConfigurationSource();
--- recurring-service/src/main/java/com/spendsmart/recurring/config/CorsConfig.java
+++ recurring-service/src/main/java/com/spendsmart/recurring/config/CorsConfig.java
@@ -1,3 +1,27 @@
+//package com.spendsmart.recurring.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.*;
+//
+//@Configuration
+//public class CorsConfig {
+//
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedOrigins("http://localhost:4200","https://spend-smart-frontend-silk.vercel.app")
+//                        .allowedMethods("*")
+//                        .allowedHeaders("*");
+//            }
+//        };
+//    }
+//}
+
+
 package com.spendsmart.recurring.config;
 
 import org.springframework.context.annotation.Bean;
@@ -9,13 +33,17 @@ public class CorsConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
+
         return new WebMvcConfigurer() {
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200","https://spend-smart-frontend-silk.vercel.app")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("*")
-                        .allowedHeaders("*");
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
